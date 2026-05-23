5acaa1a69c2ba2632b089bf0cff1cf9cc949ff1a
anaclaragamberini <ana.gamberini@edu.unifil.br>
2026-05-23 17:30:16 -0300

    Remove EmailNotificador class implementation

--- EmailNotificador.java
+++ EmailNotificador.java
@@ -1,7 +1,1 @@
-package org.solid.DIP;
 
-public class EmailNotificador implements Notificador {
-    public void enviar(String mensagem) {
-        System.out.println("Email enviado: " + mensagem);
-    }
-}
